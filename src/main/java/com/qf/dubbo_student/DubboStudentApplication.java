f2ba9ba5f54a4ba7a56fd73883c82e12f0af027c
bc <123@qq.com>
2019-07-01 16:39:31 +0800

    Initial commit

---
+++ src/main/java/com/qf/dubbo_student/DubboStudentApplication.java
@@ -0,0 +1,13 @@
+package com.qf.dubbo_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboStudentApplication.class, args);
+    }
+
+}
